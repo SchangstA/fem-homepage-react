5c2e2e0248314aea4012d1f0250741f55dc7679f
Justin Schang <theschangsta@yahoo.com>
2023-12-11 00:07:26 -0500

    front end mentor project built from scratch used personal skeleton css built seperate function for exporting images

---
+++ src/images/index.js
@@ -0,0 +1,11 @@
+const images = {
+    logo: require('./logo.svg').default,
+    iconMenuClose: require('./icon-menu-close.svg').default,
+    iconMenu: require('./icon-menu.svg').default,
+    gamingGrowth: require('./image-gaming-growth.jpg'),
+    retroPcs: require('./image-retro-pcs.jpg'),
+    topLaptops: require('./image-top-laptops.jpg'),
+    desktop: require('./image-web-3-desktop.jpg')
+}
+
+export default images;
